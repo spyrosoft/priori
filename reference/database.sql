56dd132e3f96c607f6e8c93d86d105136ec76dd8
Bennett Roesch <meta@spyrosoft.com>
2017-03-05 20:01:10 -0800

    Store tasks as a single json value per user in the database.

--- reference/database.sql
+++ reference/database.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS user_sessions;
-DROP TABLE IF EXISTS user_tasks;
 DROP TABLE IF EXISTS users;
 
 
@@ -7,7 +6,8 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	email_hash BYTEA UNIQUE NOT NULL,
 	password_hash BYTEA NOT NULL,
-	password_salt BYTEA NOT NULL
+	password_salt BYTEA NOT NULL,
+	tasks JSON
 );
 
 
@@ -15,16 +15,4 @@ CREATE TABLE user_sessions (
 	user_id INT REFERENCES users(id),
 	token TEXT NOT NULL,
 	created_at TIMESTAMP DEFAULT 'now'
-);
-
-
-CREATE TABLE user_tasks (
-	id SERIAL PRIMARY KEY,
-	user_id INT REFERENCES users(id),
-	parent_id INT,
-	task TEXT NOT NULL,
-	short_term INT NOT NULL,
-	long_term INT NOT NULL,
-	urgency INT NOT NULL,
-	difficulty INT NOT NULL
 );
--- reference/test-data.sql
+++ reference/test-data.sql
@@ -1,7 +1,8 @@
 \i database.sql
 
 INSERT INTO users (email_hash, password_hash, password_salt) VALUES ('\x6a67524f5d3117d9481dd39fdcffcde682b262e8ebbf64a81a26207413b178d6', '\x228640fa2cdf5cbf6a3e7964ac3035e59a62ddd113a4729b0d2057f2f79e703e', '\x305f94a6b2fb3de2897d');
-INSERT INTO user_tasks (user_id, task, short_term, long_term, urgency, difficulty) VALUES (1, 'My Projects', 10, 10, 0, 4);
+
+--INSERT INTO user_tasks (user_id, task, short_term, long_term, urgency, difficulty) VALUES (1, 'My Projects', 10, 10, 0, 4);
 
 
 -- with recursive all_posts (id, parentid, root_id) as (
